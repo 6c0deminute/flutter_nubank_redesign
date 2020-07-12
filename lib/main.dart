85b2c3248833f80af453542f96176052153ff675
Caio Oliveira <caiomcoliveira@gmail.com>
2020-07-12 09:51:14 -0300

    Exacly like the video

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 Color kPrimaryColor = Color(0xFF81249F);
 Color kSecondaryColor = Color(0xFF9140A9);
+
 void main() {
   runApp(MyApp());
 }
@@ -17,18 +18,18 @@ class MyApp extends StatelessWidget {
           primaryColor: kPrimaryColor,
           visualDensity: VisualDensity.adaptivePlatformDensity,
         ),
-        home: NubankHome());
+        home: NubankHomePage());
   }
 }
 
-class NubankHome extends StatefulWidget {
-  const NubankHome({Key key}) : super(key: key);
+class NubankHomePage extends StatefulWidget {
+  NubankHomePage({Key key}) : super(key: key);
 
   @override
-  _NubankHomeState createState() => _NubankHomeState();
+  _NubankHomePageState createState() => _NubankHomePageState();
 }
 
-class _NubankHomeState extends State<NubankHome> {
+class _NubankHomePageState extends State<NubankHomePage> {
   double opacity = 0.0;
   @override
   Widget build(BuildContext context) {
@@ -47,7 +48,7 @@ class _NubankHomeState extends State<NubankHome> {
                 Icons.exit_to_app,
                 color: Colors.white,
               ),
-              onPressed: null)
+              onPressed: null),
         ],
       ),
       body: Container(
@@ -67,7 +68,7 @@ class _NubankHomeState extends State<NubankHome> {
                   color: Colors.purple.withOpacity(opacity),
                 ),
               ),
-              BottomDrawer(),
+              BottomDrawer()
             ],
           ),
         ),
@@ -76,10 +77,198 @@ class _NubankHomeState extends State<NubankHome> {
   }
 }
 
+class AccountDetails extends StatelessWidget {
+  const AccountDetails({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: kSecondaryColor,
+      width: double.infinity,
+      padding: EdgeInsets.only(top: 15),
+      child: Column(
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                child: Image.asset(
+                  "nubank_logo_white.png",
+                  width: 30,
+                  height: 30,
+                ),
+              ),
+              Text(
+                "CodeMinute",
+                style: TextStyle(
+                  fontSize: 31,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+          Divider(
+            thickness: 2.5,
+            color: Colors.white.withAlpha(30),
+          ),
+          AccountBalance(),
+          SizedBox(
+            height: 12,
+          ),
+          Row(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(left: 38.0),
+                child: Text(
+                  "Actvities",
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: Container(
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30), color: Colors.white),
+              padding: EdgeInsets.all(10),
+              height: 400,
+              child: Activities(),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class AccountBalance extends StatelessWidget {
+  const AccountBalance({Key key}) : super(key: key);
+
+  _buildAccountDetails({String value, String label}) {
+    return Container(
+      padding: EdgeInsets.all(5),
+      child: Column(
+        children: <Widget>[
+          RichText(
+            text: TextSpan(style: TextStyle(color: Colors.white), children: [
+              TextSpan(
+                text: "\$",
+                style: TextStyle(fontSize: 16),
+              ),
+              TextSpan(text: value, style: TextStyle(fontSize: 30))
+            ]),
+          ),
+          Text(
+            label,
+            style: TextStyle(color: Colors.white),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+              child: _buildAccountDetails(
+                  label: "Account Balance", value: "300.32")),
+          Expanded(
+              child: _buildAccountDetails(
+                  label: "Current Bill", value: "1223.11")),
+        ],
+      ),
+    );
+  }
+}
+
+class Activities extends StatelessWidget {
+  const Activities({Key key}) : super(key: key);
+
+  _buildActivityItem(
+      {String title,
+      String subtitle,
+      String trailing,
+      IconData icon,
+      Color color = Colors.black}) {
+    return ListTile(
+      leading: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 8.0),
+        child: Icon(
+          icon,
+          color: color,
+        ),
+      ),
+      title: Text(
+        title,
+        style: TextStyle(color: color, fontWeight: FontWeight.bold),
+      ),
+      subtitle: Text(
+        subtitle,
+        style: TextStyle(color: color),
+      ),
+      trailing: Text(
+        trailing,
+        style: TextStyle(color: color),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: ListView(
+        children: <Widget>[
+          _buildActivityItem(
+              title: "Uber Drive",
+              subtitle: "\$ 5.32",
+              trailing: "Today",
+              icon: Icons.train),
+          _buildActivityItem(
+              title: "Mall",
+              subtitle: "\$ 122.50",
+              trailing: "Yesterday",
+              icon: Icons.home),
+          _buildActivityItem(
+              title: "Expensive Tickets",
+              subtitle: "\$ 399.99",
+              trailing: "4 JUL",
+              icon: Icons.airplanemode_active),
+          _buildActivityItem(
+              title: "Payment Received",
+              subtitle: "\$  612.32",
+              trailing: "1 JUL",
+              icon: Icons.monetization_on,
+              color: Colors.green[700]),
+          _buildActivityItem(
+              title: "Bill closed",
+              subtitle: "\$  612.32",
+              trailing: "1 JUL",
+              icon: Icons.monetization_on,
+              color: Colors.red[700]),
+          _buildActivityItem(
+              title: "Uber",
+              subtitle: "\$ 12.32",
+              trailing: "27 JUN",
+              icon: Icons.monetization_on),
+          _buildActivityItem(
+              title: "Uber",
+              subtitle: "\$ 14.50",
+              trailing: "27 JUN",
+              icon: Icons.monetization_on)
+        ],
+      ),
+    );
+  }
+}
+
 class BottomDrawer extends StatelessWidget {
-  const BottomDrawer({
-    Key key,
-  }) : super(key: key);
+  const BottomDrawer({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -89,26 +278,27 @@ class BottomDrawer extends StatelessWidget {
         minChildSize: 0.18,
         builder: (BuildContext context, ScrollController scrollController) {
           return SingleChildScrollView(
-            physics: ClampingScrollPhysics(),
             controller: scrollController,
+            physics: ClampingScrollPhysics(),
             child: Column(
               children: <Widget>[
                 Container(
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(40),
-                        topRight: Radius.circular(40),
-                      ),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(40),
+                      topRight: Radius.circular(40),
                     ),
-                    child: Center(
-                      child: Container(
-                        width: 150,
-                        height: 3,
-                        color: Colors.purple,
-                      ),
+                  ),
+                  child: Center(
+                    child: Container(
+                      width: 150,
+                      height: 3,
+                      color: kPrimaryColor,
                     ),
-                    height: 30),
+                  ),
+                  height: 30,
+                ),
                 Container(
                   color: Colors.white,
                   child: Column(
@@ -119,17 +309,11 @@ class BottomDrawer extends StatelessWidget {
                           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                           children: <Widget>[
                             OptionsItem(
-                              icon: Icons.money_off,
-                              label: "Split Value",
-                            ),
+                                icon: Icons.money_off, label: "Split Value"),
                             OptionsItem(
-                              icon: Icons.attach_money,
-                              label: "Donation",
-                            ),
+                                icon: Icons.attach_money, label: "Donation"),
                             OptionsItem(
-                              icon: Icons.thumb_up,
-                              label: "Leave Like",
-                            ),
+                                icon: Icons.thumb_up, label: "Leave Like")
                           ],
                         ),
                       ),
@@ -139,13 +323,11 @@ class BottomDrawer extends StatelessWidget {
                           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                           children: <Widget>[
                             OptionsItem(
-                                label: "Loans", icon: Icons.screen_share),
+                                icon: Icons.screen_share, label: "Loans"),
                             OptionsItem(
-                                label: "Charge", icon: Icons.assignment_late),
+                                icon: Icons.assignment_late, label: "Charge"),
                             OptionsItem(
-                              label: "Virtual Card",
-                              icon: Icons.credit_card,
-                            ),
+                                icon: Icons.credit_card, label: "Virtual Card")
                           ],
                         ),
                       ),
@@ -155,43 +337,30 @@ class BottomDrawer extends StatelessWidget {
                           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                           children: <Widget>[
                             OptionsItem(
-                              label: "Deposit",
-                              icon: Icons.account_balance_wallet,
-                            ),
-                            OptionsItem(
-                              label: "Please",
-                              icon: Icons.settings,
-                            ),
-                            OptionsItem(
-                              label: "Subscribe",
-                              icon: Icons.help,
-                            ),
+                                icon: Icons.account_balance_wallet,
+                                label: "Deposit"),
+                            OptionsItem(icon: Icons.settings, label: "Please"),
+                            OptionsItem(icon: Icons.help, label: "Subscribe")
                           ],
                         ),
                       ),
                       Padding(
-                        padding: const EdgeInsets.only(top: 15.0, bottom: 5),
+                        padding: const EdgeInsets.only(top: 15.0, bottom: 5.0),
                         child: Row(
                           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                           children: <Widget>[
+                            OptionsItem(icon: Icons.lock, label: "Lock Card"),
                             OptionsItem(
-                              label: "Lock Card",
-                              icon: Icons.lock,
-                            ),
+                                icon: Icons.people, label: "Invite Friends"),
                             OptionsItem(
-                              label: "Invite Friends",
-                              icon: Icons.people,
-                            ),
-                            OptionsItem(
-                              label: "Set Limit",
-                              icon: Icons.add_shopping_cart,
-                            ),
+                                icon: Icons.add_shopping_cart,
+                                label: "Set Limit")
                           ],
                         ),
-                      )
+                      ),
                     ],
                   ),
-                ),
+                )
               ],
             ),
           );
@@ -203,6 +372,7 @@ class OptionsItem extends StatelessWidget {
   const OptionsItem({Key key, this.icon, this.label}) : super(key: key);
   final IconData icon;
   final String label;
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -224,200 +394,6 @@ class OptionsItem extends StatelessWidget {
             label,
             style: TextStyle(fontSize: 11, fontWeight: FontWeight.bold),
           )
-        ],
-      ),
-    );
-  }
-}
-
-class AccountDetails extends StatelessWidget {
-  const AccountDetails({Key key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      color: kSecondaryColor,
-      padding: EdgeInsets.only(top: 15),
-      width: double.infinity,
-      child: Column(
-        children: <Widget>[
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                child: Image.asset(
-                  "nubank_logo_white.png",
-                  width: 30,
-                  height: 30,
-                ),
-              ),
-              Text(
-                "CodeMinute",
-                style: TextStyle(
-                    fontSize: 31,
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold),
-              ),
-            ],
-          ),
-          Divider(
-            thickness: 2.5,
-            color: Colors.white.withAlpha(30),
-          ),
-          Row(
-            children: <Widget>[
-              Expanded(
-                  child: BalanceDetails(
-                value: "323.50",
-                label: "Account Balance",
-              )),
-              Expanded(
-                  child: BalanceDetails(
-                value: "1,500.32",
-                label: "Current Bill",
-              )),
-            ],
-          ),
-          SizedBox(height: 12,),
-          Row(
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.only(left: 38.0),
-                child: Text("Activities", style: TextStyle(color: Colors.white),),
-              ),
-            ],
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16.0),
-            child: Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(30),
-                  color: Colors.white,
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black.withAlpha(100),
-                      spreadRadius: 1,
-                      blurRadius: 5,
-                      offset: Offset(3, 3), // changes position of shadow
-                    ),
-                  ],
-                ),
-                padding: EdgeInsets.all(10),
-                margin: EdgeInsets.only(bottom: 25),
-                height: 400,
-                child: Activity()),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class BalanceDetails extends StatelessWidget {
-  const BalanceDetails(
-      {Key key, this.value, this.label, this.color = Colors.white})
-      : super(key: key);
-  final String value;
-  final String label;
-  final Color color;
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(5),
-      // color: Colors.white,
-      child: Column(
-        children: <Widget>[
-          RichText(
-            text: TextSpan(style: TextStyle(color: color), children: [
-              TextSpan(text: "\$", style: TextStyle(fontSize: 16)),
-              TextSpan(text: value, style: TextStyle(fontSize: 30))
-            ]),
-          ),
-          Text(
-            label,
-            style: TextStyle(color: color),
-          )
-        ],
-      ),
-    );
-  }
-}
-
-class Activity extends StatelessWidget {
-  const Activity({Key key}) : super(key: key);
-
-  _buildActivityItemCustom(
-      {String title,
-      String subtitle,
-      String trailing,
-      IconData icon,
-      Color color = Colors.black}) {
-    return ListTile(
-      title: Text(
-        title,
-        style: TextStyle(fontWeight: FontWeight.bold, color: color),
-      ),
-      leading: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 8.0),
-        child: Icon(
-          icon,
-          color: color,
-        ),
-      ),
-      subtitle: Text(
-        subtitle,
-        style: TextStyle(color: color),
-      ),
-      trailing: Text(
-        trailing,
-        style: TextStyle(color: color),
-      ),
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: ListView(
-        children: <Widget>[
-          _buildActivityItemCustom(
-              title: "Uberbr Eats",
-              subtitle: "\$ 12.32",
-              trailing: "Today",
-              icon: Icons.train),
-          _buildActivityItemCustom(
-              title: "Mall",
-              subtitle: "\$ 122.50",
-              trailing: "Yesterday",
-              icon: Icons.home),
-          _buildActivityItemCustom(
-              title: "Expensive Tickets",
-              subtitle: "\$ 399.99",
-              trailing: "4 JUL",
-              icon: Icons.airplanemode_active),
-          _buildActivityItemCustom(
-              title: "Payment Received",
-              subtitle: "\$  612.32",
-              trailing: "1 JUL",
-              icon: Icons.monetization_on,
-              color: Colors.green[700]),
-          _buildActivityItemCustom(
-              title: "Bill closed",
-              subtitle: "\$  612.32",
-              trailing: "1 JUL",
-              icon: Icons.monetization_on,
-              color: Colors.red[700]),
-          _buildActivityItemCustom(
-              title: "Uber",
-              subtitle: "\$ 12.32",
-              trailing: "27 JUN",
-              icon: Icons.monetization_on),
-          _buildActivityItemCustom(
-              title: "Uber",
-              subtitle: "\$ 14.50",
-              trailing: "27 JUN",
-              icon: Icons.monetization_on)
         ],
       ),
     );
